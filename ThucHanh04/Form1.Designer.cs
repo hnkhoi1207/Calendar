fd9666ca2e41de64c1db9c518eaa7faded0aa661
NGUYEN KHOI HOANG <khoihoang1207@gmail.com>
2021-08-10 00:54:22 +0700

    first commit

---
+++ ThucHanh04/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ThucHanh04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNgay = new System.Windows.Forms.Label();
+            this.lblNam = new System.Windows.Forms.Label();
+            this.lblThang = new System.Windows.Forms.Label();
+            this.lbldanhngon = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblNgayAm = new System.Windows.Forms.Label();
+            this.lblThangAm = new System.Windows.Forms.Label();
+            this.lblNamAm = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblThu = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNgay
+            // 
+            this.lblNgay.BackColor = System.Drawing.Color.Transparent;
+            this.lblNgay.Font = new System.Drawing.Font("Segoe UI Semibold", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgay.ForeColor = System.Drawing.Color.White;
+            this.lblNgay.Location = new System.Drawing.Point(8, 89);
+            this.lblNgay.Name = "lblNgay";
+            this.lblNgay.Size = new System.Drawing.Size(253, 104);
+            this.lblNgay.TabIndex = 2;
+            this.lblNgay.Text = "4";
+            this.lblNgay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNam
+            // 
+            this.lblNam.BackColor = System.Drawing.Color.Transparent;
+            this.lblNam.Font = new System.Drawing.Font("Myriad Pro Light Cond", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNam.ForeColor = System.Drawing.Color.White;
+            this.lblNam.Location = new System.Drawing.Point(12, 58);
+            this.lblNam.Name = "lblNam";
+            this.lblNam.Size = new System.Drawing.Size(249, 31);
+            this.lblNam.TabIndex = 1;
+            this.lblNam.Text = "2018";
+            this.lblNam.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblThang
+            // 
+            this.lblThang.BackColor = System.Drawing.Color.Transparent;
+            this.lblThang.Font = new System.Drawing.Font("Myriad Pro Light Cond", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThang.ForeColor = System.Drawing.Color.White;
+            this.lblThang.Location = new System.Drawing.Point(12, 12);
+            this.lblThang.Name = "lblThang";
+            this.lblThang.Size = new System.Drawing.Size(254, 46);
+            this.lblThang.TabIndex = 0;
+            this.lblThang.Text = "Tháng 6";
+            this.lblThang.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbldanhngon
+            // 
+            this.lbldanhngon.BackColor = System.Drawing.Color.Transparent;
+            this.lbldanhngon.Font = new System.Drawing.Font("Myriad Pro SemiCond", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldanhngon.ForeColor = System.Drawing.Color.White;
+            this.lbldanhngon.Location = new System.Drawing.Point(4, 276);
+            this.lbldanhngon.Name = "lbldanhngon";
+            this.lbldanhngon.Size = new System.Drawing.Size(279, 102);
+            this.lbldanhngon.TabIndex = 2;
+            this.lbldanhngon.Text = "Khôi đẹp trai - ai cũng nói vậy";
+            this.lbldanhngon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(287, 36);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(211, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Myanmar Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(287, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "VIEW CALENDAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNgayAm
+            // 
+            this.lblNgayAm.BackColor = System.Drawing.Color.Transparent;
+            this.lblNgayAm.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblNgayAm.Font = new System.Drawing.Font("Segoe UI Semibold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgayAm.ForeColor = System.Drawing.Color.White;
+            this.lblNgayAm.Location = new System.Drawing.Point(0, 0);
+            this.lblNgayAm.Name = "lblNgayAm";
+            this.lblNgayAm.Size = new System.Drawing.Size(213, 55);
+            this.lblNgayAm.TabIndex = 1;
+            this.lblNgayAm.Text = "6";
+            this.lblNgayAm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblThangAm
+            // 
+            this.lblThangAm.BackColor = System.Drawing.Color.Transparent;
+            this.lblThangAm.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblThangAm.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThangAm.ForeColor = System.Drawing.Color.White;
+            this.lblThangAm.Location = new System.Drawing.Point(0, 55);
+            this.lblThangAm.Name = "lblThangAm";
+            this.lblThangAm.Size = new System.Drawing.Size(213, 36);
+            this.lblThangAm.TabIndex = 2;
+            this.lblThangAm.Text = "Tháng giêng";
+            this.lblThangAm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNamAm
+            // 
+            this.lblNamAm.BackColor = System.Drawing.Color.Transparent;
+            this.lblNamAm.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblNamAm.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamAm.ForeColor = System.Drawing.Color.White;
+            this.lblNamAm.Location = new System.Drawing.Point(0, 91);
+            this.lblNamAm.Name = "lblNamAm";
+            this.lblNamAm.Size = new System.Drawing.Size(213, 43);
+            this.lblNamAm.TabIndex = 3;
+            this.lblNamAm.Text = "Nhâm Ngọ";
+            this.lblNamAm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.lblNamAm);
+            this.panel2.Controls.Add(this.lblThangAm);
+            this.panel2.Controls.Add(this.lblNgayAm);
+            this.panel2.Location = new System.Drawing.Point(287, 285);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(217, 146);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblThu
+            // 
+            this.lblThu.BackColor = System.Drawing.Color.Transparent;
+            this.lblThu.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu.ForeColor = System.Drawing.Color.White;
+            this.lblThu.Location = new System.Drawing.Point(6, 214);
+            this.lblThu.Name = "lblThu";
+            this.lblThu.Size = new System.Drawing.Size(255, 42);
+            this.lblThu.TabIndex = 5;
+            this.lblThu.Text = "Thứ hai";
+            this.lblThu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(515, 439);
+            this.Controls.Add(this.lblThu);
+            this.Controls.Add(this.lblNgay);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblNam);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblThang);
+            this.Controls.Add(this.lbldanhngon);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNgay;
+        private System.Windows.Forms.Label lblNam;
+        private System.Windows.Forms.Label lblThang;
+        private System.Windows.Forms.Label lbldanhngon;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNgayAm;
+        private System.Windows.Forms.Label lblThangAm;
+        private System.Windows.Forms.Label lblNamAm;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblThu;
+    }
+}
+
---
+++ ThucHanh04/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ThucHanh04
+{
+    public partial class Form1 : Form
+    {
+        List<string> lst = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CultureInfo cultureInfo = new CultureInfo("vi-VN");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+        }
+
+        int ly, lm, ld, d, m;
+
+        void getAL(DateTime d)
+        {
+            VietnameseCalendar.FromDateTime(d, out ly, out lm, out ld);
+            lblNgayAm.Text = ld.ToString();
+
+            string t = lm.ToString();
+            if (t == "1") t = "Giêng";
+            else if (t == "12") t = "Chạp";
+            lblThangAm.Text = "Tháng " + t;
+
+            string y = VietnameseCalendar.GetYearName(ly).ToString();
+            lblNamAm.Text = "Năm " + y;
+        }
+
+        void readData()
+        {
+            string path = Application.StartupPath + @"\quotes.txt";
+            string line;
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                lst.Add(line);
+            }
+            file.Close();
+        }
+
+        void update(DateTime current)
+        {
+            lblNgay.Text = current.ToString("dd");
+            lblThang.Text = "Tháng " + current.ToString("MM");
+            lblNam.Text = current.ToString("yyyy");
+            DayOfWeek thu = current.DayOfWeek;
+            string sthu = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(thu);
+            lblThu.Text = sthu;
+            d = current.Day;
+            m = current.Month;
+            
+            
+        }
+
+        void updanhngon()
+        {
+            //Random rand = new Random();
+            //lbldanhngon.Text = lst[rand.Next(lst.Count)];
+            Random ran = new Random();
+            int so = ran.Next(0,310);
+            lbldanhngon.Text = lst[so];
+        }
+
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+            update(dateTimePicker1.Value);
+            getAL(dateTimePicker1.Value);
+            updanhngon();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Visible = true;
+            button1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readData();
+            update(DateTime.Now);
+            getAL(DateTime.Now);
+            updanhngon();
+        }
+
+         
+
+       
+
+        
+    }
+}
